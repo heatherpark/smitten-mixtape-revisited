b211ddd13cb0107cae2b465e796c8868415334e3
Heather Park <hthr.prk@gmail.com>
2017-10-24 12:08:24 -0700

    Make files for track request handlers and request handlers index

---
+++ server/config/request-handlers/index.js
@@ -0,0 +1,3 @@
+export { addTrackToPlaylist } from './track-request-handlers';
+export { deleteTrackFromPlaylist } from './track-request-handlers';
+export { fetchAllTracks } from './track-request-handlers';
---
+++ server/config/request-handlers/track-request-handlers.js
@@ -0,0 +1,11 @@
+export const addTrackToPlaylist = (req, res) => {
+  res.sendStatus(200);
+};
+
+export const deleteTrackFromPlaylist = (req, res) => {
+  res.sendStatus(200);
+};
+
+export const fetchAllTracks = (req, res) => {
+  res.sendStatus(200);
+};
