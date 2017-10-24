1c1bd4dd5f792c58fa74bacbb380cae400cca656
Heather Park <hthr.prk@gmail.com>
2017-10-24 12:27:57 -0700

    Remove unnecessary semicolons

--- server/config/request-handlers/track.js
+++ server/config/request-handlers/track.js
@@ -1,11 +1,11 @@
 export function addTrackToPlaylist(req, res) {
   res.sendStatus(200);
-};
+}
 
 export function deleteTrackFromPlaylist(req, res) {
   res.sendStatus(200);
-};
+}
 
 export function fetchAllTracks(req, res) {
   res.sendStatus(200);
-};
+}
