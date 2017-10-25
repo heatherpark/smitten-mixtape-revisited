79059d6866e0ad239f689ce2e1e5eaa3e52fb22b
Heather Park <hthr.prk@gmail.com>
2017-10-25 13:07:41 -0700

    Update routes file to use import *

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,12 @@
-import { addPlaylist, addTrackToPlaylist, deleteTrackFromPlaylist, fetchAllTracks, fetchPlaylist } from '../request-handlers';
+import * as requestHandlers from '../request-handlers';
 
 export default (app, express) => {
   // playlist
-  app.post('/playlist/:playlistId', addPlaylist);
-  app.get('/playlist/:playlistId', fetchPlaylist);  
+  app.post('/playlist/:playlistId', requestHandlers.addPlaylist);
+  app.get('/playlist/:playlistId', requestHandlers.fetchPlaylist);  
 
   // tracks
-  app.post('/playlist/:playlistId/track', addTrackToPlaylist);
-  app.delete('/playlist/:playlistId/track/:trackId', deleteTrackFromPlaylist);  
-  app.get('/tracks', fetchAllTracks)
+  app.post('/playlist/:playlistId/track', requestHandlers.addTrackToPlaylist);
+  app.delete('/playlist/:playlistId/track/:trackId', requestHandlers.deleteTrackFromPlaylist);  
+  app.get('/tracks', requestHandlers.fetchAllTracks)
 };
