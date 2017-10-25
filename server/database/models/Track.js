4eb719de1291531e0e490bdfb3006bf0c866568a
Heather Park <hthr.prk@gmail.com>
2017-10-25 13:08:02 -0700

    Write MongoDB model for Track

---
+++ server/database/models/Track.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const trackSchema = new mongoose.Schema({
+  trackId: Number,
+  user: String,
+  title: String,
+  permalink: String,
+  image: String,
+  duration: Number
+});
+
+export default mongoose.model('Playlist', trackSchema);
